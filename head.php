a402e71640262fc657452d1493e9f04a53b5e04c
acotton <acotton@TH-B03-VMWKS07.tmdci.local>
2019-01-14 16:01:15 -0800

    head echo

--- head.php
+++ head.php
@@ -5,3 +5,5 @@
 
 <link href="https://fonts.googleapis.com/css?family=Josefin+Slab" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet">
+
+<?php echo("head loaded");?>
