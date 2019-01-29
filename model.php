153d851f15d9460406ca8a8a8c92972a232d8e7c
cot15011@byui.edu <acotton@TH-B03-VMWKS07.tmdci.local>
2019-01-28 16:54:17 -0800

    building staff_login query

--- model.php
+++ model.php
@@ -6,28 +6,32 @@
 function staff_login($lastName, $idNumber) {
     echo "inside staff_login function</br>";
     echo "lastName = " . $lastName . " and idNumber = " . $idNumber . "</br>";
-    require ('waConnect.php');
+    $serverName = "TH-B03-VMWKS07\SQLEXPRESS";  //WORK
+    $connectionInfo = array("Database" => "beta_torresmartinez");
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    //require ('waConnect.php');
 //    echo "</br>";
 //    echo $conn;
 //    echo "</br>";
 //    echo $serverName;
-    $tsql = "SELECT [StaffID]
+    $sql = "SELECT [StaffID]
         ,[FirstName]
         ,[LastName]
         FROM [beta_torresmartinez].[StaffModule].[Staff]
         WHERE [StaffID] = 4";
     //WHERE [StaffID] = " . $idNumber;
-    $stmt = sqlsrv_query($conn, $tsql);
+    $stmt = sqlsrv_query($conn, $sql);
     echo "</br>";
-    echo $tsql;
+    echo $sql;
     echo "</br>";
     echo $stmt;
     if ($stmt == false) {
         echo "Error in query preparation/execution.\n";
         die(print_r(sqlsrv_errors(), true));
     }
-    $name = sqlsrv_get_field($stmt, 0);
-    echo "$name: ";
-    $comment = sqlsrv_get_field($stmt, 1);
-    echo $comment;
+    sqlsrv_close( $conn );
+//    $name = sqlsrv_get_field($stmt, 0);
+//    echo "$name: ";
+//    $comment = sqlsrv_get_field($stmt, 1);
+//    echo $comment;
 }
